1c2419a77f07883e018a4e5e84921fa06e6ec3ac
Manoj <manoj.pawane@bridgelabz.com>
2020-11-14 20:10:01 +0530

    added code

---
+++ ProgramInCSharp/Program.cs
@@ -0,0 +1,72 @@
+﻿using ProgramInCSharp;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello workd");
+        int ch = 0;
+        do
+        {
+            Console.WriteLine("1. Exit");
+            Console.WriteLine("2. Positive and Negative number");
+            Console.WriteLine("3. Even and Odd number");
+            Console.WriteLine("4. Sum of first N natural numbers");
+            Console.WriteLine("5. Sum of Number in range");
+            Console.WriteLine("6. Greatest of two numbers");
+            Console.WriteLine("7. Greatest of Three numbers");
+            Console.WriteLine("8. Prime number");
+            Console.WriteLine("9. Prime Number in range..");
+            Console.WriteLine("10. Sum of Digits..");
+            Console.WriteLine("11. Reverse of Number.");
+            Console.WriteLine("12. Palindrome checker..");
+            Console.WriteLine("13. Check Palindrome for number.");
+            Console.WriteLine("14. Armstrong Number..");
+            Console.WriteLine("15. Armstrong Number within range..");
+            Console.WriteLine("16. Fibonacci Series..");
+            Console.WriteLine("17. Factors of Number.");
+            ch = Convert.ToInt32(Console.ReadLine());
+            switch(ch)
+            {
+                case 2:Utility.PostiveAndNegativeNumber();
+                        break;
+                case 3:
+                    Utility.EvenOddNumber();
+                    break;
+                case 4:
+                    Utility.SumOfFirstNNaturalNumber();
+                    break;
+                case 5:
+                    Utility.SumOfNumbersInRange();
+                    break;
+                case 6:
+                    Utility.GreatestOfTwoNumber();
+                    break;
+                case 7:Utility.GreatestOfThreeNumber();
+                    break;
+                case 8: Utility.PrimeNumber();
+                    break;
+                case 9:Utility.PrimeNumberInRange();
+                    break;
+                case 10: Utility.SumOfDigitsOfANumber();
+                    break;
+                case 11: Utility.ReverseOfANumber();
+                    break;
+                case 12:Utility.CheckPalindrome();
+                    break;
+                case 13: Utility.CheckPalindromeForNumber();
+                    break;
+                case 14: Utility.ArmstrongNumber();
+                    break;
+                case 15: Utility.ArmstrongNumberWithinRange();
+                    break;
+                case 16: Utility.FibonacciSeries();
+                    break;
+                case 17:Utility.FactorialOfNumber();
+                    break;
+            }
+        }
+        while (ch != 1 );
+    }
+}
---
+++ ProgramInCSharp/Utility.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramInCSharp
+{
+    public class Utility
+    {
+        public static void PostiveAndNegativeNumber()
+        {
+            int a = 5;
+            if(a> 0)
+            {
+                Console.WriteLine("Postive Number");
+            }
+            else
+            {
+                Console.WriteLine("Negative Number");
+            }
+        }
+
+        public static void EvenOddNumber()
+        {
+            int a = 5;
+            if(a % 2 == 0)
+            {
+                Console.WriteLine("Number is even Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd number");
+            }
+        }
+
+        public static void SumOfFirstNNaturalNumber()
+        {
+            int a = 10;
+            int sum = 0;
+            for(int i = 1; i<=a; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Sum of N natural number are: {0}", sum);
+        }
+
+        public static void SumOfNumbersInRange()
+        {
+            int start = 4;
+            int end = 20;
+            int sum = 0;
+            for(int i = start; i <=end; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of numbers in range:{0} ", sum);
+        }
+
+        public static void GreatestOfTwoNumber()
+        {
+            int a = 5;
+            int b = 10;
+            if(a > b)
+            {
+                Console.WriteLine("Greatest Number:{0}",a);
+            }
+            else
+            {
+                Console.WriteLine("Greatest Number: {0}", b);
+            }
+        }
+
+        public static void GreatestOfThreeNumber()
+        {
+            int a = 5;
+            int b = 7;
+            int c = 10;
+            if(a > b && a > c)
+            {
+                Console.WriteLine(a+" is greatest number..");
+            }
+            else if(b > a && b > c)
+            {
+                Console.WriteLine(b+ " is greatest number..");
+            }
+            else if(c > b && c > a)
+            {
+                Console.WriteLine(c+ " is greatest number..");
+            }
+            else
+            {
+                Console.WriteLine("All numbers are same..");
+            }
+        }
+
+        public static void LeapYear()
+        {
+            int year = 3000;
+            if(year % 4 ==0 && year%100 != 0 || year % 400 ==0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap year..");
+            }
+        }
+
+        public static void PrimeNumber()
+        {
+            int a = 5;
+            for(int i = 2; i < a; i++)
+            {
+                if(a % i ==0)
+                {
+                    Console.WriteLine("Number is not prime");
+                    return;
+                }
+            }
+            Console.WriteLine("Number is prime..");
+        }
+
+        public static void PrimeNumberInRange()
+        {
+            int lowRange = 2;
+            int highRange = 100;
+            int flag = 0;
+            for (int i = lowRange; i < highRange; i++)
+            {
+                flag = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag != 1)
+                {
+                    Console.WriteLine(i+" ");
+                }
+            }
+        }
+
+        public static void SumOfDigitsOfANumber()
+        {
+            int number = 12345;
+            int sum = 0;
+            while(number != 0 )
+            {
+                int temp = number % 10;
+                sum = sum + temp;
+                number = number / 10;
+            }
+            Console.WriteLine("Sum of Digits: "+sum);
+        }
+
+        public static void ReverseOfANumber()
+        {
+            int number = 12345;
+            int temp = 0;
+            int temp2 = 0;
+            while(number != 0)
+            {
+                temp2 = number % 10;
+                temp = temp * 10 + temp2;
+                number = number / 10;
+            }
+
+            Console.WriteLine(temp);
+        }
+
+        public static void CheckPalindrome()
+        {
+            string st = "Madama";
+            string st1 = "";
+            int length = st.Length-1;
+            for(int i = length; i >= 0; i--)
+            {
+                st1 = st1 + st[i];
+            }
+            if(String.Equals(st, st1, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("String are palindrome..");
+            }
+            else
+            {
+                Console.WriteLine("Strings are not palindrome");
+            }
+        }
+
+        public static void CheckPalindromeForNumber()
+        {
+            long st = 121;
+            long temp = 0;
+            long temp2 = 0;
+            long duplicate = st;
+            while(st!=0)
+            {
+                temp = st % 10;
+                temp2 = temp2 * 10 + temp;
+                st = st / 10;
+            }
+            if(temp2 == duplicate)
+            {
+                Console.WriteLine("Palindrome..");
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrome..");
+            }
+        }
+
+        public static void ArmstrongNumber()
+        {
+            int a = 407;
+            int cubeValue = 0;
+            int duplicateNumber = a;
+            int temp = 0;
+            while(a != 0)
+            {
+                temp = a % 10;
+                cubeValue = cubeValue + (int)Math.Pow(temp, 3);
+                a = a / 10;
+            }
+            if(cubeValue == duplicateNumber)
+            {
+                Console.WriteLine("Armstrong Number..");
+            }
+            else
+            {
+                Console.WriteLine("Not Armstrong Number");
+            }
+        }
+
+        public static void ArmstrongNumberWithinRange()
+        {
+            int startRange = 10;
+            int endingRange = 1000;
+            for(int i =startRange; i < endingRange; i ++)
+            {
+                int cubeValue = 0;
+                int tempDuplicate = i;
+                int temp = 0;
+                while (tempDuplicate != 0)
+                {
+                    temp = tempDuplicate % 10;
+                    cubeValue += (int)Math.Pow(temp, 3);
+                    tempDuplicate /= 10;
+                }
+                if (cubeValue == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void FibonacciSeries()
+        {
+            int limit = 10;
+            int z = 1;
+            int initial = 0;
+            int currentValue = 0;
+            while(currentValue < limit)
+            {
+                if(currentValue <= 1)
+                {
+                    Console.WriteLine(currentValue);
+                }
+                else
+                {
+                    currentValue = initial + z;
+                    Console.WriteLine(currentValue + " ");
+                    initial = z;
+                    z = currentValue;
+                }
+
+            }
+        }
+
+        public static void FactorialOfNumber()
+        {
+            int number = 5;
+            int factor = 1;
+            for(int i = 1; i <=number; i++)
+            {
+                factor = factor * i;
+            }
+            Console.WriteLine(factor);
+        }
+
+    }
+}
